8a8df2bc2daf38af4393eb39c6e7b89108f903ac
Dipak_kasar <kasardipak333@gmail.com>
2023-02-24 22:33:23 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,6 +4,7 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("Enter you text here...");
 
+
     const handleOnChange = (e) => {
         setText(e.target.value)
     }
@@ -12,11 +13,14 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
         props.showAlert("Text changed to uppercase!", "success : ")
+
     }
     const handleLowClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
         props.showAlert("Text changed to lowercase!", "success : ")
+
+
     }
     const handleCapitalizedClick = () => {
         let arr = text.split(' ');
@@ -26,17 +30,23 @@ export default function TextForm(props) {
         let newText = newArr.join(" ");
         setText(newText)
         props.showAlert("Capitalized the text!", "success : ")
+
+
     }
-    const removeExtraSpaces = () => {
+    const removeExtraSpaces = (e) => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "))
         props.showAlert("Removed all extra spacess!", "success : ")
+
     }
     const handleClearClick = () => {
-        let newText = "";
+
+        let newText = ""
         setText(newText)
         props.showAlert("Text cleared!", "success : ")
+
     }
+
 
     return (
         <div className='vh-100' style={props.changeMode}>
@@ -45,17 +55,17 @@ export default function TextForm(props) {
                 <div className='pb-3' >
                     <textarea className="form-control" rows="10" value={text} onChange={handleOnChange} style={props.changeMode} />
                 </div>
-                <button className={`btn btn-outline-${props.changeMode.btnStyle} mx-1`} onClick={handleUpClick}>Change to uppercase</button>
-                <button className={`btn btn-outline-${props.changeMode.btnStyle} mx-1`} onClick={handleLowClick}>Change to lowercase</button>
-                <button className={`btn btn-outline-${props.changeMode.btnStyle} mx-1`} onClick={handleCapitalizedClick}>Capitalized text</button>
-                <button className={`btn btn-outline-${props.changeMode.btnStyle} mx-1`} onClick={removeExtraSpaces}>Remove Extra Spaces</button>
-                <button className={`btn btn-outline-${props.changeMode.btnStyle} mx-1`} onClick={handleClearClick}>Clear text</button>
+                <button disabled={text.length === 0} className={`btn btn-outline-${props.changeMode.btnStyle} mx-1 my-2`} onClick={handleUpClick}>Change to uppercase</button>
+                <button disabled={text.length === 0} className={`btn btn-outline-${props.changeMode.btnStyle} mx-1 my-2`} onClick={handleLowClick}>Change to lowercase</button>
+                <button disabled={text.length === 0} className={`btn btn-outline-${props.changeMode.btnStyle} mx-1 my-2`} onClick={handleCapitalizedClick}>Capitalized text</button>
+                <button disabled={text.length === 0} className={`btn btn-outline-${props.changeMode.btnStyle} mx-1 my-2`} onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+                <button disabled={text.length === 0} className={`btn btn-outline-${props.changeMode.btnStyle} mx-1 my-2`} onClick={handleClearClick}>Clear text</button>
             </div>
             <div className="container my-3 ">
                 <h2><i>Text Summary</i></h2>
-                <p>Your text have {text.length === 1 ? 0 : (text.split(/[ ]+/).length - 1)} words and {text.length} characters </p>
+                <p>Your text have {text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters </p>
                 <h2 className='my-3'><i>Preview</i></h2>
-                <p >{text.length > 0 ? text : `Enter text above to preview`}</p>
+                <p >{text.length > 0 ? text : `Nothing to preview`}</p>
             </div>
         </div>
     )
